2872f997ae893c1f2b4cb3980d476b846c282bf2
frederickbolin <frederickbolin@Fredericks-MacBook-Pro.local>
2020-04-14 04:45:40 -0700

    initial import. Had to reconnect my github to file again becuase somehow it disconnented

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -9,7 +9,7 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
       label ?
       (<label className={`${otherProps.value.length ? "shrink" : ""} form-input-label`}
       >
-      {/* Breakdown for this Scope: This <label> will always have the className{form-input-label}, but Ive also added the shrink property whenever the user has typed anything in, its mainly for certain broswer to auto-complete when user get t the portain of signin page. */}
+      {/* Breakdown for this Scope: This <label> will always have the className{form-input-label}, but Ive also added the shrink property whenever the user has typed anything in, its mainly for certain broswer to auto-complete when user get to the portain of signin page. */}
         {label}
 
       </label>)
