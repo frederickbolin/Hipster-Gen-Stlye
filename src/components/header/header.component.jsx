3f2bb7f59357153ffd5ad7fd84e6f71f85cb217a
frederickbolin <frederickbolin@Fredericks-MacBook-Pro.local>
2020-05-07 17:49:03 -0700

    Npm installed styled-components to code some css into js. Change custom-button.component.jsx with import and export const of styling for buttons.  In header.component I remove link because it was no longer needed and add all export const from header.styles.jsx into header.component and replaced div and links with styled-components(CSS in JS). ALso applied middleware to store.js to render logs/tools for development and not production with notes.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -13,33 +12,33 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 // Because this isnt a JSX file we dont actually set the import, Im just importing in this crown.svg as ReactComponent key word but setting that as LOGO key word for now.
 // now by doing this. I can now place <Logo></Logo> anywhere on this file.
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      <Link className='option' to='/shop'>
+      </OptionLink>
+      <OptionLink to='/shop'>
         CONTACT
-      </Link>
+      </OptionLink>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionLink>
       ) : (
-        <Link className='option' to='/signin'>
+        <OptionLink to='/signin'>
           SIGN IN
-        </Link>
+        </OptionLink>
       )}
       <CartIcon />
-    </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
+    </OptionsContainer>
+    {hidden ? null : <CartDropdown />} 
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,7 @@ const middlewares = [];
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
+// This middleware scope helps me in production stage side not for users to see the developer log/tools middlewares(URL search). But for the developer side It should show the development logs and tools(npm or yarn start). Writing process.env.NODE_ENV helps recognize rather its in production or development stage in order to render the Logs/Tools.
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
